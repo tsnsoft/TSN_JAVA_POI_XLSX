f6ac5547d1cfe6991c6bb9458f66be809a8d6da9
proffix4 <proffix4@gmail.com>
2020-02-28 13:27:20 +0600

    update

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -6,21 +6,34 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
 import org.apache.poi.ss.usermodel.CellType;
-
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class JavaApplication1 {
+    final static String EXCEL_FILE_NAME = "test.xlsx";
 
-    public static void readXLSXFile() throws IOException {
-        InputStream ExcelFileToRead = new FileInputStream("Test.xlsx");
-        XSSFWorkbook wb = new XSSFWorkbook(ExcelFileToRead);
-        XSSFWorkbook test = new XSSFWorkbook();
+    // Запись данных в XLSX-файл
+    public static void writeXLSXFile() throws IOException {
+        XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream(EXCEL_FILE_NAME));
         XSSFSheet sheet = wb.getSheetAt(0);
-        XSSFRow row;
-        XSSFCell cell;
+        for (int r = 0; r < 3; r++) {
+            XSSFRow row = sheet.createRow(r+9);
+            for (int c = 0; c < 3; c++) {
+                row.createCell(c + 8).setCellValue(String.valueOf(2*(r+c)));
+            }
+        }
+        FileOutputStream fos = new FileOutputStream(EXCEL_FILE_NAME);
+        wb.write(fos); fos.flush(); fos.close();
+    }
+
+     // Чтение данных из XLSX-файла
+   public static void readXLSXFile() throws IOException {
+        InputStream ExcelFileToRead = new FileInputStream(EXCEL_FILE_NAME);
+        XSSFSheet sheet = new XSSFWorkbook(ExcelFileToRead).getSheetAt(0);
+        XSSFRow row; XSSFCell cell;
+        // Считывание текстовых и цифровых данных из файла
         Iterator rows = sheet.rowIterator();
         while (rows.hasNext()) {
             row = (XSSFRow) rows.next();
@@ -36,33 +49,6 @@ public class JavaApplication1 {
             }
             System.out.println();
         }
-
-    }
-
-    public static void writeXLSXFile() throws IOException {
-        String excelFileName = "Test.xlsx";
-        InputStream ExcelFileToRead = new FileInputStream(excelFileName);
-        XSSFWorkbook wb = new XSSFWorkbook(ExcelFileToRead);
-        XSSFWorkbook test = new XSSFWorkbook();
-        XSSFSheet sheet = wb.getSheetAt(0);
-
-//        String sheetName = "Sheet1";
-//        XSSFWorkbook wb = new XSSFWorkbook();
-//        XSSFSheet sheet = wb.createSheet(sheetName);
-        
-        for (int r = 0; r < 5; r++) {
-            XSSFRow row = sheet.createRow(r);
-            for (int c = 0; c < 5; c++) {
-                XSSFCell cell1 = row.createCell(c);
-                XSSFCell cell2 = row.createCell(c + 10);
-                cell1.setCellValue("Cell " + r + " " + c);
-                cell2.setCellValue(c * r);
-            }
-        }
-        FileOutputStream fileOut = new FileOutputStream(excelFileName);
-        wb.write(fileOut);
-        fileOut.flush();
-        fileOut.close();
     }
 
     public static void main(String[] args) {
